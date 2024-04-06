354545ba4d21a458696aa60dec9d00025b6238d4
mohamed elshafey <elshafeym170@gmail.com>
2024-04-06 21:44:07 +0200

    implemented interfaces and classes for domain.core

---
+++ MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime TimeStamp { get; protected set; } 
+        protected Event() { 
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
